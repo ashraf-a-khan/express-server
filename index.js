79ea34691aaaa2fc79c1aae210eea0967dc593d4
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-10-05 19:45:07 -0400

    Saves one record in mongoDB per user

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+mongoose.connect();
+require("./models/User");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
